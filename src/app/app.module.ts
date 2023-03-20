3d6c267d175ece1bc61a61367be4f996279d201d
kisuke-JP <kisuke19584181@gmail.com>
2023-03-20 16:53:56 +0900

    [update]ヘッダーを仮作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MyRoutingModule } from './my-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { HeaderComponent } from './header/header.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+  };
+
+  goInfo() {
+    this.router.navigate([''])
+  }
+}
