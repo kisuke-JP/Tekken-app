7356aa2b49d76318e448cee1692af9d950dfe918
kisuke-JP <kisuke19584181@gmail.com>
2023-03-20 12:18:03 +0900

    [update]ホームページコンポーネント作成＆Router新規作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MyRoutingModule } from './my-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MyRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
